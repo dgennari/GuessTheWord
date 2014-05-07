b2eba808ca550a5f8cba47e857d71b6addfd06db
Mattias Mohlin <mattias.mohlin@se.ibm.com>
2014-05-07 11:40:51 +0200

    Add code for generating a random word

--- server.js
+++ server.js
@@ -16,6 +16,49 @@ if (process.env.hasOwnProperty("VCAP_SERVICES")) {
 	var port = process.env.VCAP_APP_PORT;	
 }
 
+/**
+ * Lookup the word in the wordnik online dictionary and return a description for it.
+ * @param word {String} Word to lookup description for
+ * @param cb_description {function} Callback with the description as argument. 
+ * If the word was not found in the dictionary the description is empty.
+ */
+function wordLookup(word, cb_description) {
+  http.request(
+    {
+	host: "api.wordnik.com",
+	path: "/v4/word.json/" + word + "/definitions?limit=1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5"
+    },	function (res) {
+	  var str = '';
+	  res.on('data', function(d) {
+	    str += d;
+	  });
+	  res.on('end', function() {
+	    var wordList = JSON.parse(str);
+	    cb_description(wordList.length > 0 ? wordList[0].text : "");
+	});
+  }).end();	
+}
+
+app.get('/randomword', function(request, response) {
+  http.request(
+  {
+    host: "api.wordnik.com",
+    path: "/v4/words.json/randomWord?hasDictionaryDef=false&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5"
+  }, function (res) {
+	var str = '';
+	res.on('data', function(d) {
+	  str += d;
+	});
+	res.on('end', function() {
+	  var wordObj = JSON.parse(str);
+	  wordLookup(wordObj.word, function(descr) {
+	    var randomWordObj = { word : wordObj.word, description : descr };
+	    response.send(JSON.stringify(randomWordObj));		
+	});								
+     });
+  }).end();
+});
+
 //Set path to Jade template directory
 app.set('views', __dirname + '/views');
 
