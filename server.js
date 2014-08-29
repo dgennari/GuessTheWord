18aceb2a8815249e195f75d608ab2e343cff34ca
mattias <mattias.mohlin@se.ibm.com>
2014-08-29 15:03:04 +0200

    Fix spelling error in env var name

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ if (process.env.hasOwnProperty("VCAP_SERVICES")) {
   console.log('VCAP_SERVICES: %s', process.env.VCAP_SERVICES);    
 
   // Also parse out Cloudant settings.
-  cloudant = env['CloudantNoSQLDB'][0].credentials;  
+  cloudant = env['cloudantNoSQLDB'][0].credentials;  
 }
 
 var nano = require('nano')(cloudant.url);
